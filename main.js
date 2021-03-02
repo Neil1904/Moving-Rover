5fff4e5e29e5514b36fcecfff9e95669c8a80b24
Neil1904 <75314458+Neil1904@users.noreply.github.com>
2021-03-02 18:19:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function add() {
     background_imgTag = new Image();
     background_imgTag.onload = uploadBackground;
     background_imgTag.src = background_image;
-    
+
     rover_imgTag = new Image();
     rover_imgTag.onload = uploadrover;
     rover_imgTag.src = rover_image;
@@ -33,24 +33,60 @@ window.addEventListener("keydown", my_keydown);
 function my_keydown(e) {
     keyPressed = e.keyCode;
     console.log(keyPressed);
-    
-    if(keyPressed == '38'){
+
+    if (keyPressed == '38') {
         up();
         console.log("up");
     }
 
-    if(keyPressed == '40'){
+    if (keyPressed == '40') {
         down();
         console.log("down");
     }
 
-    if(keyPressed == '37'){
+    if (keyPressed == '37') {
         left();
         console.log("left");
     }
 
-    if(keyPressed == '39'){
+    if (keyPressed == '39') {
         right();
         console.log("right");
+    }
+}
+
+function up() {
+    if (rover_y >= 0) {
+        rover_y = rover_y - 10;
+        console.log("When up arrow is pressed, x= " + rover_x + "y= " + rover_y);
+        uploadBackground();
+        uploadrover();
+    }
+}
+
+function down() {
+    if (rover_y <= 500) {
+        rover_y = rover_y + 10;
+        console.log("When down arrow is pressed, x= " + rover_x + "y= " + rover_y);
+        uploadBackground();
+        uploadrover();
+    }
+}
+
+function left() {
+    if (rover_x >= 0) {
+        rover_x = rover_x - 10;
+        console.log("When left arrow is pressed, x= " + rover_x + "y= " + rover_y);
+        uploadBackground();
+        uploadrover();
+    }
+}
+
+function right() {
+    if (rover_x <= 700) {
+        rover_x = rover_x + 10;
+        console.log("When right arrow is pressed, x= " + rover_x + "y= " + rover_y);
+        uploadBackground();
+        uploadrover();
     }
 }
